329de0fe5a987c4499c0f9f5f005814ef1d6d296
Eugen Sabelfeld <sabelfeld@gmx.de>
2021-10-07 13:30:27 +0200

    Figuren werden in Startformation aufgestellt

--- setze-modified.php
+++ setze-modified.php
@@ -6,12 +6,50 @@
     global $color;
     global $figure;
 
-    
+    function indexOfFigure($fig){
+        $chars = array("♜","♞","♝","♛","♚","♟","♖","♘","♗","♕","♔","♙");
+        for($k = 0; $k < 12; $k++){
+            if($fig == $chars[$k])
+                if($k < 6)
+                    return ($k + 6);
+                else
+                    return $k;
+        }
+    }
 
     function setze($c, $r ,$f, $fcolor){
         
         static $color;
         static $figuire;
+        $jBoard = "schach-pos.json";
+        $brett = file_get_contents($jBoard);
+        
+        //$chess = json_encode($brett);
+
+        $chess = array(array("♜", "♞", "♝","♚","♛","♝","♞","♜"),
+        array("♟","♟","♟","♟","♟","♟","♟","♟"),
+         array(" ", " ", " ", " "," ", " "," ", " "),
+         array(" ", " ", " ", " "," ", " "," ", " "),
+         array(" ", " ", " ", " "," ", " "," ", " "),
+         array(" ", " ", " ", " "," ", " "," ", " "),
+         array("♙","♙","♙","♙","♙","♙","♙","♙"),
+         array("♖", "♘","♗" ,"♔","♕","♗","♘","♖"));
+
+        $chessColor = array(array(array("♜","bl"),array("♞","bl"), array("♝","bl"),array("♚","bl"),array("♛","bl"),array("♝","bl"), array("♞","bl"),array("♜","bl")),
+                 array(array("♟","bl"),array("♟","bl"), array("♟","bl"),array("♟","bl"),array("♟","bl"),array("♟","bl"), array("♟","bl"),array("♟","bl")),
+                 array(array("","none"),array("","none"), array("","none"),array("","none"),array("","none"),array("","none"), array("","none"),array("","none")),
+                 array(array("","none"),array("","none"), array("","none"),array("","none"),array("","none"),array("","none"), array("","none"),array("","none")),
+                 array(array("","none"),array("","none"), array("","none"),array("","none"),array("","none"),array("","none"), array("","none"),array("","none")),
+                 array(array("","none"),array("","none"), array("","none"),array("","none"),array("","none"),array("","none"), array("","none"),array("","none")),
+                 array(array("♙","wh"),array("♙","wh"), array("♙","wh"),array("♙","wh"),array("♙","wh"),array("♙","wh"), array("♙","wh"),array("♙","wh")),
+                 array(array("♖","wh"),array("♘","wh"), array("♗","wh"),array("♔","wh"),array("♕","wh"),array("♗","wh"), array("♘","wh"),array("♖","wh")));
+
+         
+       
+        //echo "<pre>";
+        //print($chess[0][0]);
+        //echo "</pre>";
+
         $letters = array("A","B","C","D","E","F","G","H");
         $index = 0;
         for($i = 0; $i < 8; $i++){
@@ -20,13 +58,10 @@
             }
         }
 
-        $charactersFill = array("♜","♞","♝","♛","♚","♟");
+        $characters = array("♜","♞","♝","♛","♚","♟","♖","♘","♗","♕", "♔", "♙");
         $charactersNotFill = array("♖","♘","♗","♕", "♔", "♙");
         $characterIndex = 0;
-        for($i = 0; $i < 6; $i++){
-            if($f == $charactersFill[$i])
-                $characterIndex = $i;
-        }
+        
         
 
         echo "<table class='chess-board'> 
@@ -39,47 +74,54 @@
             echo"<th>{$headerLetters}</th>";
         }
         echo"</tr>";
-        for($i = 1; $i < 9;$i++){
+        //Schachbrett - Aufbau
+        for($i = 0; $i < 8;$i++){
             echo"<tr>";
-            echo"<th>{$i}</th>";
-            for($j=1; $j<9; $j++){
+            echo"<th>".($i + 1)."</th>";
+            for($j=0; $j < 8; $j++){
                 $num = (8 * ($i - 1)) + $j;
-                if(fmod($i+$j,2) == 0){
-                    if($j == $index && $i == $r) 
-                        if($fcolor == 'white'){
-                            echo"<td class='colblack light'>$charactersNotFill[$characterIndex]</td>";
-                            $color = 'white';
-                            $figuire = $charactersNotFill[$characterIndex];
-                        }
-                        else {
-                            echo"<td class='colblack light'>$charactersFill[$characterIndex]</td>";
-                            $color = 'white';
-                            $figuire = $charactersFill[$characterIndex];
-                        }
-                    else
-                        echo"<td class='light'></td>";
+                if(fmod($i+$j,2) == 0){ // <<<<<<<<<<<<<< weisses Feld
+                    if($chessColor[$i][$j][0] == ""){
+                        echo"<td class='colblack light'></td>";
                     }
-                else{
-                    if($j == $index && $i == $r) 
-                        if($fcolor == 'white')
-                          echo"<td class='colwhite dark'>$f</td>";
+                    else if($chessColor[$i][$j][1] == "bl"){
+                        $temp = $chessColor[$i][$j][0];
+                        echo"<td class='colblack light'>".$temp."</td>";
+                        
+                        }
                         else{
-                          echo"<td class='colwhite dark'>$charactersNotFill[$characterIndex]</td>";
-                          $color = 'white';
-                          $figuire = $charactersFill[$characterIndex];      
+                            
+                            echo"<td class='colblack light'>".$characters[indexOfFigure($chessColor[$i][$j][0])]."</td>";
                         }
-                    else 
-                        echo"<td class='dark'></td>";
-                    }
                 }
-                echo"<th class='rt'>{$i}</th>";
-                echo"</tr>";
-            }
-            echo"<th></th>";
-            foreach($letters As $headerLetters){
-                echo"<th class='rt'>{$headerLetters}</th>";
-            }
-        }    
+                
+                else {   
+                    
+                    if($chessColor[$i][$j][1] == "wh"){
+                        $temp = indexOfFigure($chessColor[$i][$j][0]) - 6;
+                        echo"<td class='colwhite dark'>".$characters[$temp]."</td>";
+                    }
+                    else if($chessColor[$i][$j][1] == "bl"){
+                        $temp = indexOfFigure($chessColor[$i][$j][0]);
+                        echo"<td class='colwhite dark'>".$characters[$temp]."</td>";
+                    }
+                    else{
+                        echo"<td class='colwhite dark'></td>";
+                    }  
+                        
+                   
+                     
+                }
+            }   
+        
+        echo"<th class='rt'>".($i + 1)."</th>";
+        echo"</tr>";
+        } 
+        echo"<th></th>";
+        foreach($letters As $headerLetters){
+            echo"<th class='rt'>{$headerLetters}</th>";
+        }
+    }    
     ?>
 </body>
 </html>
